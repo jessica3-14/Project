3fad20fdc070a4cdbf85a243efa0be5340f2a867
stephanielam3211 <77123518+stephanielam3211@users.noreply.github.com>
2023-11-08 22:14:46 -0600

    bubblesort for cuda

---
+++ Builds/CUDA/bubblesort.cu
@@ -0,0 +1,118 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <caliper/cali.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+int THREADS;
+int BLOCKS;
+int NUM_VALS;
+
+__device__ int d_NUM_VALS; // Declare as a device constant
+
+const char* bubble_sort_step_region = "bubble_sort_step";
+const char* entire_computation_region = "entire_computation";
+int kernel_call;
+
+void print_elapsed(clock_t start, clock_t stop)
+{
+  double elapsed = ((double) (stop - start)) / CLOCKS_PER_SEC;
+  printf("Elapsed time: %.3fs\n", elapsed);
+}
+
+float random_float()
+{
+  return (float)rand()/(float)RAND_MAX;
+}
+
+void array_fill(float *arr, int length)
+{
+  srand(time(NULL));
+  int i;
+  for (i = 0; i < length; ++i) {
+    arr[i] = random_float();
+  }
+}
+
+__global__ void bubble_sort_step(float *dev_values)
+{
+  unsigned int i = threadIdx.x + blockDim.x * blockIdx.x;
+  unsigned int next = i + 1;
+
+  if (next < d_NUM_VALS) {
+    if (dev_values[i] > dev_values[next]) {
+      float temp = dev_values[i];
+      dev_values[i] = dev_values[next];
+      dev_values[next] = temp;
+    }
+  }
+}
+
+void bubble_sort(float *values)
+{
+  float *dev_values;
+  size_t size = NUM_VALS * sizeof(float);
+
+  cudaMalloc((void**)&dev_values, size);
+  cudaMemcpy(dev_values, values, size, cudaMemcpyHostToDevice);
+
+  // Copy NUM_VALS to device constant
+  cudaMemcpyToSymbol(d_NUM_VALS, &NUM_VALS, sizeof(int));
+
+  dim3 blocks(BLOCKS, 1);    /* Number of blocks   */
+  dim3 threads(THREADS, 1);  /* Number of threads  */
+
+  CALI_MARK_BEGIN(bubble_sort_step_region);
+
+  for (int i = 0; i < NUM_VALS - 1; ++i) {
+    kernel_call++;
+    bubble_sort_step<<<blocks, threads>>>(dev_values);
+  }
+
+  cudaDeviceSynchronize();
+  CALI_MARK_END(bubble_sort_step_region);
+
+  cudaMemcpy(values, dev_values, size, cudaMemcpyDeviceToHost);
+
+  cudaFree(dev_values);
+}
+
+int main(int argc, char *argv[])
+{
+  THREADS = atoi(argv[1]);
+  NUM_VALS = atoi(argv[2]);
+  BLOCKS = NUM_VALS / THREADS;
+
+  printf("Number of threads: %d\n", THREADS);
+  printf("Number of values: %d\n", NUM_VALS);
+  printf("Number of blocks: %d\n", BLOCKS);
+
+  clock_t start, stop;
+
+  float *values = (float*)malloc(NUM_VALS * sizeof(float));
+  array_fill(values, NUM_VALS);
+
+  CALI_MARK_BEGIN(entire_computation_region);
+
+  start = clock();
+  bubble_sort(values); /* Inplace */
+  stop = clock();
+
+  CALI_MARK_END(entire_computation_region);
+
+  print_elapsed(start, stop);
+
+  size_t size = NUM_VALS * sizeof(float);
+
+  // Calculate metrics based on kernel_call (number of iterations)
+  float data_size_gb = (kernel_call * size * 4 * (1e-9)); // Size in GB
+  float kernel_execution_time_s = (float)(stop - start) / CLOCKS_PER_SEC; // Kernel execution time in seconds
+  float effective_bandwidth_gb_s = (data_size_gb) / kernel_execution_time_s;
+  printf("Effective Bandwidth (GB/s): %.6fGB/s\n", effective_bandwidth_gb_s);
+
+  free(values);
+
+  return 0;
+}
