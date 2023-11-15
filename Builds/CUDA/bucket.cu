0adfc7dcda1666179303072167e4fefdfad81da9
Jessica Virginia Williams <jessicavwlms@login1.cluster>
2023-11-15 12:24:16 -0600

    bucket improvements

--- Builds/CUDA/bucket.cu
+++ Builds/CUDA/bucket.cu
@@ -5,7 +5,8 @@
 #include <caliper/cali.h>
 #include <caliper/cali-manager.h>
 #include <thrust/sort.h>
-
+#include <adiak.hpp>
+#include <chrono>
 
 __global__ void bucketSort(double* input, double* output,int* d_sizes, int numElements, int numBuckets) {
 
@@ -36,16 +37,15 @@ const char* comp_small = "comp_small";
 const char* correctness_check = "correctness_check";
 
     int numElements = atoi(argv[2]);
-
     int numBuckets = atoi(argv[1]);
     int mode = atoi(argv[3]);
-    
+
+float gpu_time=0;
     cali::ConfigManager mgr;
     mgr.start();
     CALI_MARK_BEGIN(main);
     CALI_MARK_BEGIN(data_init);
     thrust::host_vector<double> h_input(numElements,0);
-
 
     genData(numElements,mode,thrust::raw_pointer_cast(h_input.data()));
     CALI_MARK_END(data_init);
@@ -62,20 +62,30 @@ const char* correctness_check = "correctness_check";
 
     // Launch the kernel to distribute elements into buckets
 
-    int blockSize = 256;
+    int blockSize = numBuckets;
 
     int numBlocks = (numElements + blockSize - 1) / blockSize;
     CALI_MARK_BEGIN(comp);
     CALI_MARK_BEGIN(comp_large);
+auto start = std::chrono::high_resolution_clock::now();
     bucketSort<<<numBlocks, blockSize>>>(thrust::raw_pointer_cast(d_input.data()), thrust::raw_pointer_cast(d_output.data()),thrust::raw_pointer_cast(d_sizes.data()), numElements, numBuckets);
+    cudaDeviceSynchronize();
+auto stop = std::chrono::high_resolution_clock::now();
+auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+gpu_time+= duration.count() /1e6;
 CALI_MARK_END(comp_large);
 CALI_MARK_END(comp);
 
     CALI_MARK_BEGIN(comp);
     CALI_MARK_BEGIN(comp_large);
+start = std::chrono::high_resolution_clock::now();
 for(int i=0;i<numBuckets;i++){
 thrust::sort(thrust::device,d_output.begin()+numElements*i,d_output.begin()+numElements*i+d_sizes[i],thrust::less<double>());
 }
+    cudaDeviceSynchronize();
+stop = std::chrono::high_resolution_clock::now();
+duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+gpu_time+= duration.count() /1e6;
     CALI_MARK_END(comp_large);
     CALI_MARK_END(comp);
 
@@ -107,6 +117,42 @@ CALI_MARK_BEGIN(correctness_check);
     }
 CALI_MARK_END(correctness_check);
 CALI_MARK_END(main);
+
+adiak::init(NULL);
+
+adiak::launchdate();    // launch date of the job
+
+adiak::libraries();     // Libraries used
+
+adiak::cmdline();       // Command line used to launch the job
+
+adiak::clustername();   // Name of the cluster
+
+adiak::value("Algorithm", "bucket"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+
+adiak::value("ProgrammingModel", "CUDA"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
+
+adiak::value("Datatype", "double"); // The datatype of input elements (e.g., double, int, float)
+
+adiak::value("SizeOfDatatype", sizeof(double)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+
+adiak::value("InputSize", numElements); // The number of elements in input dataset (1000)
+
+adiak::value("InputType",mode); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
+
+adiak::value("num_procs", numBuckets); // The number of processors (MPI ranks)
+adiak::value("gpu_time", gpu_time);
+//adiak::value("num_threads", num_threads); // The number of CUDA or OpenMP threads
+
+//adiak::value("num_blocks", num_blocks); // The number of CUDA blocks 
+
+adiak::value("group_num", 12); // The number of your group (integer, e.g., 1, 10)
+
+adiak::value("implementation_source", "AI"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
+
+
+
+
 mgr.stop();
    mgr.flush();
 
--- Builds/MPI/bucket.cpp
+++ Builds/MPI/bucket.cpp
@@ -1,25 +1,37 @@
-
 #include <inttypes.h>
+
 #include <stdlib.h>
+
 #include <stdio.h>
+
 #include <math.h>
+
 #include <mpi.h>
-#include "helper.cpp"
+#include <caliper/cali.h>
 
+#include<caliper/cali-manager.h>
 
+#include <adiak.hpp>
 
 // Comparison function used by qsort
+
 int compare_dbls(const void* arg1, const void* arg2)
+
 {
+
     double a1 = *(double *) arg1;
+
     double a2 = *(double *) arg2;
+
     if (a1 < a2) return -1;
+
     else if (a1 == a2) return 0;
+
     else return 1;
+
 }
-
-
 // verification function
+
 int verify(double *array, int array_len)
 {
    int i;
@@ -31,9 +43,6 @@ int verify(double *array, int array_len)
    return 1;
 }
 
-
-
-// Sort the array in place
 void qsort_dbls(double *array, int array_len)
 {
     qsort(array, (size_t)array_len, sizeof(double),
@@ -41,138 +50,229 @@ void qsort_dbls(double *array, int array_len)
 }
 
 
-
 int main( int argc, char *argv[] )
 {
-   
+CALI_CXX_MARK_FUNCTION;
    MPI_Init(&argc, &argv);
 
    int n, i;
-   double *local_input_array;   //The input array for each processor
-   double *local_bucketlist;    //This array will contain input elements in order of the processors for each processor
-                                //E.g elements of process 0 will be stored first, then elements of process 1, and so on
-   int *local_sscounts;         //This array will contain the counts of elements each processor will send to others
-   int *local_rscounts;         //This array will contain the counts of elements each processor will receive from others
-   int *local_sdspls;           //The offsets in bucketlist where the elements of different processes will be stored - send side
-   int *local_rdspls;           //The offsets in local_array where the elements of different processes will be stored - receive side
-   double *local_array;         //This array will contain the corrsponding elements in each process
-   int *local_bin_elements;     //It will keep track of how many elements have been included in the pth bin
-   int *local_array_sizes;      //number of ellements each process has to sort
-   int *fdspls;                 //final offset in sorted_array
-   double *sorted_array;        //final sorted array
+
+   double *input_array;   //the input array
+
+   double *bucketlist;    //this array will contain input elements in order of the processors
+
+                          //e.g elements of process 0 will be stored first, then elements of process 1, and so on
+
+   int *scounts;          //This array will contain the counts of elements each processor will receive
+
+   int *dspls;            //The rel. offsets in bucketlist where the elements of different processes will be stored
+
+   double *local_array;   //This array will contain the elements in each process
+
+   int *bin_elements;     //it will keep track of how many elements have been included in the pth bin
+
+   double *sorted_array;  //final sorted array
 
 
+int numElements = atoi(argv[1]);
+int mode = atoi(argv[2]);
 
+const char* main = "main";
+
+const char* data_init = "data_init";
+
+const char* comm = "comm";
+
+const char* comm_small = "comm_small";
+
+const char* comm_large = "comm_large";
+
+const char* comp = "comp";
+
+const char* comp_large = "comp_large";
+
+const char* comp_small = "comp_small";
+
+const char* correctness_check = "correctness_check";
 
    int p, rank;
+
    MPI_Comm_size(MPI_COMM_WORLD, &p);
+
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-   local_sscounts = new int[p];
-   local_rscounts = new int[p];
-   local_sdspls = new int[p];
-   local_rdspls = new int[p];
-   fdspls = new int[p];
+int numBuckets = p;
 
-   
-  n = atoi(argv[1]);
-  int mode = atoi(argv[2]); 
-      
-   local_input_array = new double[n/p];
-   //CALI_MARK_BEGIN(data_init);
-   genData(n/p,mode,local_input_array);
-   //CALI_MARK_END(data_init);
-   local_bucketlist = new double[n/p];
+cali::ConfigManager mgr;
 
-   local_bin_elements = new int[p];
-   local_array_sizes = new int[p];
+    mgr.start();
 
-   
-   //CALI_MARK_BEGIN(comp_large);
-   for(i = 0 ; i < p ; i++)
-   {
-      local_sscounts[i] = 0 ;
-   }
-   
-   
-   for(i = 0 ; i < n/p ; i++)
-   {
-      local_sscounts[(int)(local_input_array[i]/(1.0/p))]++;
-   }
-   
-   for(i = 0 ; i<p ; i++)
-   {
-      local_bin_elements[i] = local_sscounts[i];
-   }
-    
-   local_sdspls[0] = 0;
-   for(i = 0 ; i< p-1 ;i++)
-   {
-      local_sdspls[i+1] = local_sdspls[i] + local_sscounts[i];
-   }
-        
-   int bin;
-   int pos;
-   for(i = 0 ; i < n/p ; i++)
-   {
-      bin = (int)(local_input_array[i]/(1.0/p));
-      pos = local_sdspls[bin] + local_sscounts[bin] - local_bin_elements[bin];
-      local_bucketlist[pos] = local_input_array[i];
-      local_bin_elements[bin]--;
-   }
-   //CALI_MARK_END(comp_large);
-
-   //CALI_MARK_BEGIN(comm_small);
-   MPI_Allreduce(local_sscounts, local_array_sizes, p, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-   local_array = new double[local_array_sizes[rank]];
-
-   MPI_Alltoall(local_sscounts,1, MPI_INT,local_rscounts,1, MPI_INT,MPI_COMM_WORLD);
-   //CALI_MARK_END(comm_small);
-   //CALI_MARK_BEGIN(comp_small);
-   local_rdspls[0] = 0;
-   for(i = 0 ; i< p-1 ;i++)
-   {
-      local_rdspls[i+1] = local_rdspls[i] + local_rscounts[i];
-   }
-   //CALI_MARK_END(comp_small);
-   
-   
-   //CALI_MARK_BEGIN(comm_large);
-   MPI_Alltoallv(local_bucketlist, local_sscounts, local_sdspls, MPI_DOUBLE, local_array,local_rscounts, local_rdspls, MPI_DOUBLE, MPI_COMM_WORLD);
-   //CALI_MARK_END(comm_large);
-   
-
-   //CALI_MARK_BEGIN(comp_large);
-   qsort_dbls(local_array, local_array_sizes[rank]);
-   //CALI_MARK_END(comp_large);
-   sorted_array = new double[n];
+   scounts = new int[p];
+   dspls = new int[p];
 
 
-   fdspls[0] = 0;
-   for(i = 0 ; i< p-1 ;i++)
-   {
-      fdspls[i+1] = fdspls[i] + local_array_sizes[i];
-   }
-   //CALI_MARK_BEGIN(comm_large);
-   MPI_Gatherv(local_array, local_array_sizes[rank], MPI_DOUBLE, 
-               sorted_array, local_array_sizes, fdspls, MPI_DOUBLE,0, MPI_COMM_WORLD); 
-
-   //CALI_MARK_END(comm_large);
+CALI_MARK_BEGIN(data_init);
    if (rank == 0)
    {
-      //CALI_MARK_BEGIN(correctness_check);
-      if (verify(sorted_array, n-n%p))
-         printf("Successful sort\n");
-      else
-        printf("Unsuccessful sort\n");
-      //CALI_MARK_END(correctness_check);
+
+      if (argc == 1)
+
+      {
+         fprintf(stderr, "ERROR: Please specify the number of elements.\n");
+         exit(1);
+      }
+
+      n = atoi(argv[1]);
+      input_array = new double[n];
+
+      bucketlist = new double[n];
+      bin_elements = new int[p];
+
+
+      for(i = 0 ; i < n ; i++)
+      {
+        input_array[i] = ((double) rand()/RAND_MAX);
+      }
+      for(i = 0 ; i < p ; i++)
+      {
+         scounts[i] = 0 ;
+      }
+
+      for(i = 0 ; i < n ; i++)
+
+      {
+
+         scounts[(int)(input_array[i]/(1.0/p))]++;
+
+      }
+
+
+
+      for(i = 0 ; i<p ; i++)
+
+      {
+
+         bin_elements[i] = scounts[i];
+
+      }
+
+      dspls[0] = 0;
+      for(i = 0 ; i< p-1 ;i++)
+      {
+         dspls[i+1] = dspls[i] + scounts[i];
+      }
+
+
+      int bin;
+      int pos;
+      for(i = 0 ; i < n ; i++)
+      {
+
+         bin = (int)(input_array[i]/(1.0/p));
+
+         pos = dspls[bin] + scounts[bin] - bin_elements[bin];
+
+         bucketlist[pos] = input_array[i];
+
+         bin_elements[bin]--;
+
+      }
+
 
    
+
    }
+CALI_MARK_END(data_init);
 
 
-  
+ 
+
+CALI_MARK_BEGIN(comm);
+CALI_MARK_BEGIN(comm_small);
+   MPI_Bcast(scounts, p, MPI_INT, 0, MPI_COMM_WORLD); 
+
+   MPI_Bcast(dspls, p, MPI_INT, 0, MPI_COMM_WORLD); 
+
+   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+CALI_MARK_END(comm_small);
+
+
+
+   local_array = new double[scounts[rank]];
+   sorted_array = new double[n];
+
+CALI_MARK_BEGIN(comm_large);
+   MPI_Scatterv(bucketlist, scounts, dspls, MPI_DOUBLE,
+                local_array, scounts[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+CALI_MARK_END(comm_large);
+CALI_MARK_END(comm);
+
+CALI_MARK_BEGIN(comp);
+CALI_MARK_BEGIN(comp_large);
+   qsort_dbls(local_array, scounts[rank]);
+CALI_MARK_END(comp_large);
+CALI_MARK_END(comp);
+
+CALI_MARK_BEGIN(comm);
+CALI_MARK_BEGIN(comm_large);
+   MPI_Gatherv(local_array, scounts[rank], MPI_DOUBLE, 
+               sorted_array, scounts, dspls, MPI_DOUBLE,0, MPI_COMM_WORLD); 
+CALI_MARK_END(comm_large);
+CALI_MARK_END(comm);
+
+
+CALI_MARK_BEGIN(correctness_check);
+   if (rank == 0)
+   {
+
+      if (verify(sorted_array, n)){
+         printf("Successful sort\n");
+}
+      else{
+        printf("Unsuccessful sort\n");
+}
+CALI_MARK_END(correctness_check);
+
+   }
    MPI_Finalize();
+adiak::init(NULL);
+
+adiak::launchdate();    // launch date of the job
+
+adiak::libraries();     // Libraries used
+
+adiak::cmdline();       // Command line used to launch the job
+
+adiak::clustername();   // Name of the cluster
+
+adiak::value("Algorithm", "bucket"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+
+adiak::value("ProgrammingModel", "MPI"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
+
+adiak::value("Datatype", "double"); // The datatype of input elements (e.g., double, int, float)
+
+adiak::value("SizeOfDatatype", sizeof(double)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+
+adiak::value("InputSize", numElements); // The number of elements in input dataset (1000)
+
+adiak::value("InputType",mode); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
+
+adiak::value("num_procs", numBuckets); // The number of processors (MPI ranks)
+
+//adiak::value("num_threads", num_threads); // The number of CUDA or OpenMP threads
+//
+////adiak::value("num_blocks", num_blocks); // The number of CUDA blocks 
+//
+adiak::value("group_num", 12); // The number of your group (integer, e.g., 1, 10)
+//
+adiak::value("implementation_source", "AI");
+
+
+
+
+
+mgr.stop();
+
+   mgr.flush();
    return 0;
-   
 }
