690f39ec13ba2ba95f915c8a756134a9c9b76a0d
stephanielam3211 <77123518+stephanielam3211@users.noreply.github.com>
2023-11-15 23:52:04 -0600

    bubblesort cuda

--- Builds/CUDA/bubblesort.cu
+++ Builds/CUDA/bubblesort.cu
@@ -8,14 +8,14 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+#include "helper.cu"
+
 int THREADS;
 int BLOCKS;
 int NUM_VALS;
 
 __device__ int d_NUM_VALS; // Declare as a device constant
 
-//const char* bubble_sort_step_region = "bubble_sort_step";
-//const char* entire_computation_region = "entire_computation";
 int kernel_call;
 
 void print_elapsed(clock_t start, clock_t stop)
@@ -65,17 +65,25 @@ void bubble_sort(float *values)
 {
   float *dev_values;
   size_t size = NUM_VALS * sizeof(float);
+  
+  CALI_MARK_BEGIN("comm");
 
   cudaMalloc((void**)&dev_values, size);
+  
+  CALI_MARK_BEGIN("comm_large");
   cudaMemcpy(dev_values, values, size, cudaMemcpyHostToDevice);
+  CALI_MARK_END("comm_large");
 
   // Copy NUM_VALS to device constant
   cudaMemcpyToSymbol(d_NUM_VALS, &NUM_VALS, sizeof(int));
+  
+  CALI_MARK_END("comm");
 
   dim3 blocks(BLOCKS, 1);    /* Number of blocks   */
   dim3 threads(THREADS, 1);  /* Number of threads  */
 
-  CALI_MARK_BEGIN("comp_large");
+  CALI_MARK_BEGIN("comp");
+  CALI_MARK_BEGIN("comp_small");
 
   for (int i = 0; i < NUM_VALS - 1; ++i) {
     kernel_call++;
@@ -83,35 +91,54 @@ void bubble_sort(float *values)
   }
 
   cudaDeviceSynchronize();
-  CALI_MARK_END("comp_large");
+  CALI_MARK_END("comp_small");
+  CALI_MARK_END("comp");
+  
+  CALI_MARK_BEGIN("comm");
+  CALI_MARK_BEGIN("comm_large");
 
   cudaMemcpy(values, dev_values, size, cudaMemcpyDeviceToHost);
+  
+  CALI_MARK_END("comm_large");
+  CALI_MARK_END("comm");
 
   cudaFree(dev_values);
 }
 
 int main(int argc, char *argv[])
 {
+  CALI_MARK_BEGIN("main");
   THREADS = atoi(argv[1]);
   NUM_VALS = atoi(argv[2]);
+  int mode = atoi(argv[3]);
   BLOCKS = NUM_VALS / THREADS;
 
   printf("Number of threads: %d\n", THREADS);
   printf("Number of values: %d\n", NUM_VALS);
   printf("Number of blocks: %d\n", BLOCKS);
+  
+  cali::ConfigManager mgr;
+  mgr.start();
 
-  CALI_MARK_BEGIN("comp");
+  
   clock_t start, stop;
   CALI_MARK_BEGIN("data_init");
+  double *temp_values = (double *)malloc(NUM_VALS * sizeof(double));
+  genData(NUM_VALS, mode, temp_values, THREADS);
   float *values = (float*)malloc(NUM_VALS * sizeof(float));
-  array_fill(values, NUM_VALS);
+  //array_fill(values, NUM_VALS);
+  for(int i = 0; i < NUM_VALS; i++){
+    values[i] = (float)temp_values[i];
+  }
   CALI_MARK_END("data_init");
+  free(temp_values);
   
-
+  CALI_MARK_BEGIN("comp");
+  CALI_MARK_BEGIN("large_comp");
   start = clock();
   bubble_sort(values); /* Inplace */
   stop = clock();
-
+  CALI_MARK_END("large_comp");
   CALI_MARK_END("comp");
 
   print_elapsed(start, stop);
@@ -123,6 +150,21 @@ int main(int argc, char *argv[])
   float kernel_execution_time_s = (float)(stop - start) / CLOCKS_PER_SEC; // Kernel execution time in seconds
   float effective_bandwidth_gb_s = (data_size_gb) / kernel_execution_time_s;
   printf("Effective Bandwidth (GB/s): %.6fGB/s\n", effective_bandwidth_gb_s);
+  
+  adiak::init(NULL);
+  adiak::launchdate();
+  adiak::libraries();
+  adiak::cmdline();
+  adiak::clustername();
+  adiak::value("Algorithm", "Bubblesort");
+  adiak::value("ProgrammingModel", "CUDA");
+  adiak::value("Datatype", "float");
+  adiak::value("SizeOfDatatype", 8);
+  adiak::value("InputSize", NUM_VALS);
+  adiak::value("InputType", mode);
+  adiak::value("num_threads", THREADS);
+  adiak::value("group_num", 12);
+  adiak::value("implementation_source", "https://www.geeksforgeeks.org/bubble-sort/");
 
   CALI_MARK_BEGIN("correctness_check");
   if(verify(values)){
@@ -134,6 +176,10 @@ int main(int argc, char *argv[])
   CALI_MARK_END("correctness_check");
 
   free(values);
+  CALI_MARK_END("main");
+  
+  mgr.stop();
+  mgr.flush();
 
   return 0;
 }
